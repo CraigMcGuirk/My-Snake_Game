6a4eca2512a379931e06de8375a2533a9b6d1384
CraigMcGuirk <craigmcguirkw@gmail.com>
2023-04-18 01:58:27 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.sertup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+
+segments = []
+
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new.segment)
+
+game_is_on = True
+while game_is_on:
+    for seg in segments:
+        seg.forward(20)
+        screen.update()
+        time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
